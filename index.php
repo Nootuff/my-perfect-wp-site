6f869917b87dd7434cb3e06f300865a925dbccc5
Adam Walker <adamwalkerlondon@gmail.com>
2022-12-30 20:46:24 +0000

    content-exerpt post template created

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ get_template_part('template-parts/banner', 'title');
 //Another wordpress function. It pulls in banner-title, note the way the title is pulled in, you can learn more about this function in the wordpress docs.
 ?>
 
-  <!--<section class="title-banner">
+<!--<section class="title-banner">
     <div class="container">
       <div class="row">
         <div class="col-md-10 offset-md-1 col-sm-12 offset-sm-0 overflow-hidden text-center">
@@ -25,26 +25,26 @@ get_template_part('template-parts/banner', 'title');
         </div>
         <div class="col-sm-6">
           <form action="index.html" class="" method="post">
-          	<div class="row">
-          		<div class="col-lg-8">
-          			<input name="test" type="text" value="">
-          		</div>
-          		<div class="col-lg-4">
-          			<button class="btn btn-invert m-0" name="button" type="button">Subscribe</button>
-          		</div>
-          	</div>
+            <div class="row">
+              <div class="col-lg-8">
+                <input name="test" type="text" value="">
+              </div>
+              <div class="col-lg-4">
+                <button class="btn btn-invert m-0" name="button" type="button">Subscribe</button>
+              </div>
+            </div>
           </form>
         </div>
       </div>
     </div>
   </section>-->
 
-  <div class="content-area">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 offset-md-2 overflow-hidden">
+<div class="content-area">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2 overflow-hidden">
 
-          <article class="post">
+        <!-- <article class="post">
             <div class="meta">
               <span>Sep 24, 2021</span>
             </div>
@@ -86,14 +86,28 @@ get_template_part('template-parts/banner', 'title');
               Test Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
             </p>
             <a href="#" class="read-more">Read the full post -&gt;</a>
-          </article>
+          </article> -->
 
-        </div>
+        <?php
+
+        if (have_posts()) { //If there are posts in the blog database...
+          while (have_posts()) { //While you still have posts in the database...
+        
+            the_post(); //WP function, pulls each individual post's data from the loop.
+        
+            get_template_part('template-parts/content', 'exerpt'); //the first argument is the 'slug', this line of code takes the html template from the content-exerpt file.
+        
+          }
+        }
+
+        ?>
+
       </div>
     </div>
   </div>
+</div>
 
-  <?
+<?
 get_footer();
 //This is a wordpress function that looks for a a footer.php file in the root of your theme file.
 ?>
---
+++ template-parts/content-exerpt.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is The Individual post file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package bootstrap2wordpress
+ * @since Bootstrap to WordPress 2.0
+ */
+?>
+
+<article class="post my-5">
+            <div class="meta">
+              <span>Sep 24, 2021</span>
+            </div>
+            <h2><a href="#">Blog post title</a></h2>
+            <p class="excerpt">
+              Test Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
+            </p>
+            <a href="#" class="read-more">Read the full post -&gt;</a>
+          </article>

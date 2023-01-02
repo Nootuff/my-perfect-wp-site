bfc8f2919134bbede431822d70f0e363174baf35
Adam Walker <adamwalkerlondon@gmail.com>
2023-01-02 12:10:36 +0000

    404 page template created

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * The template for displaying a 404 page.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package bootstrap2wordpress
+ * @since 2.0
+ */
